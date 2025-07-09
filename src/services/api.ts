e3152d8fe4b807602f7e5662c84fd2479dd733ab
danieltwentynine <danielmcardoso2016@protonmail.com>
2025-07-09 19:06:46 -0300

    usando jest

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
 import { Game } from '../App'
 
 const api = createApi({
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
-
+import { combineReducers, configureStore, PreloadedState} from '@reduxjs/toolkit'
 import carrinhoReducer from './reducer/carrinho'
-
 import api from '../services/api'
 
-export const store = configureStore({
-  reducer: {
-    carrinho: carrinhoReducer,
-    [api.reducerPath]: api.reducer
-  },
-  middleware: (getDefauletMiddelware) =>
-    getDefauletMiddelware().concat(api.middleware)
+const rootReducer = combineReducers({
+  carrinho: carrinhoReducer,
+  [api.reducerPath]: api.reducer
 })
 
-export type RootReducer = ReturnType<typeof store.getState>
+export function configuraStore(preloadedState?: PreloadedState<RootState>) {
+  return configuraStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware: () => any[]) =>
+      getDefaultMiddleware().concat(api.middleware),
+    preloadedState
+  })
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof configuraStore>
